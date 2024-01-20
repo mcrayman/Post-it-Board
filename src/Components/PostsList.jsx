ae5a98c90c99ba522d80637f1fa30d275b3d3261
Matt McMeans <mrmcmans@memphis.edu>
2024-01-20 17:03:15 -0600

    Add new post option

--- src/Components/PostsList.jsx
+++ src/Components/PostsList.jsx
@@ -1,13 +1,34 @@
+import NewPost from './NewPost';
 import Post from './Post';
 import classes from './PostsList.module.css';
+import { useState } from 'react';
 
 function PostsList() {
-    return ( 
-        <ul className={classes.posts}>
-            <Post author="Maximillian" body="React.js is awesome"/>
-            <Post author="Matthew" body="React.js is great"/>
-        </ul>
-     );
+  const [enteredBody, setEnteredBody] = useState('');
+  const [enteredAuthor, setEnteredAuthor] = useState('');
+
+  function bodyChangeHandler(event) {
+    setEnteredBody(event.target.value);
+  }
+
+  function authorChangeHandler(event) {
+    setEnteredAuthor(event.target.value);
+  }
+
+  return ( 
+    <>
+      <Modal>
+        <NewPost 
+          onBodyChange={bodyChangeHandler} 
+          onAuthorChange={authorChangeHandler}
+        />
+      </Modal>
+      <ul className={classes.posts}>
+        <Post author={enteredAuthor} body={enteredBody}/>
+        <Post author="Matthew" body="React.js is great"/>
+      </ul>
+    </>
+  );
 }
 
 export default PostsList;
