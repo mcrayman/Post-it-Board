68949497510282ce6708d2a6bcf8d09e67218ae0
Matt McMeans <mrmcmans@memphis.edu>
2024-01-21 15:26:24 -0600

    Add Modal

--- src/Components/PostsList.jsx
+++ src/Components/PostsList.jsx
@@ -1,34 +1,41 @@
+import Modal from './Modal';
 import NewPost from './NewPost';
 import Post from './Post';
 import classes from './PostsList.module.css';
 import { useState } from 'react';
 
 function PostsList() {
-  const [enteredBody, setEnteredBody] = useState('');
-  const [enteredAuthor, setEnteredAuthor] = useState('');
+    const [modalIsVisible, setModalIsVisible] = useState(true);
+    const [enteredBody, setEnteredBody] = useState('');
+    const [enteredAuthor, setEnteredAuthor] = useState('');
 
-  function bodyChangeHandler(event) {
-    setEnteredBody(event.target.value);
-  }
+    function hideModalHandler() {
+        setModalIsVisible(false);
+    }
+    
+    function bodyChangeHandler(event) {
+        setEnteredBody(event.target.value);
+    }
 
-  function authorChangeHandler(event) {
-    setEnteredAuthor(event.target.value);
-  }
+    function authorChangeHandler(event) {
+        setEnteredAuthor(event.target.value);
+    }
 
-  return ( 
-    <>
-      <Modal>
-        <NewPost 
-          onBodyChange={bodyChangeHandler} 
-          onAuthorChange={authorChangeHandler}
-        />
-      </Modal>
-      <ul className={classes.posts}>
-        <Post author={enteredAuthor} body={enteredBody}/>
-        <Post author="Matthew" body="React.js is great"/>
-      </ul>
-    </>
-  );
+    return (
+        <>
+            {modalIsVisible ? (
+                <Modal onClose={hideModalHandler}>
+                    <NewPost 
+                        onBodyChange={bodyChangeHandler} 
+                        onAuthorChange={authorChangeHandler}
+                    />
+                </Modal> 
+            ) : false}
+            <ul className={classes.posts}>
+                <Post author={enteredAuthor} body={enteredBody} />
+            </ul>
+        </>
+    );
 }
 
 export default PostsList;
