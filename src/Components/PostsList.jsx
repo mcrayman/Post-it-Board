284c6c4d0e8e165dc6e839f4946d2b87d33201a4
Matt McMeans <mrmcmans@memphis.edu>
2024-01-21 16:51:47 -0600

    Add default for no posts

--- src/Components/PostsList.jsx
+++ src/Components/PostsList.jsx
@@ -14,16 +14,23 @@ function PostsList({ isPosting, onStopPosting }) {
     
     return (
         <>
-        {isPosting && (
-            <Modal onClose={onStopPosting}>
-            <NewPost onCancel={onStopPosting} onAddPost={addPostHandler}/>
-            </Modal>
-        )}
-        <ul className={classes.posts}>
-            {posts.map((post) => (
-            <Post key={post.id} body={post.body} author={post.author} />
-            ))}
-        </ul>
+            {isPosting && (
+                <Modal onClose={onStopPosting}>
+                <NewPost onCancel={onStopPosting} onAddPost={addPostHandler}/>
+                </Modal>
+            )}
+            {posts.length > 0 && (
+            <ul className={classes.posts}>
+                {posts.map((post) => (
+                <Post key={post.id} body={post.body} author={post.author} />
+                ))}
+            </ul>
+            )};
+            {posts.length === 0 && (<div style={{ textAlign: 'center', color: 'white' }}>
+                <h2>There are no posts yet.</h2>
+                <p>Start adding some!</p>
+                </div>
+            )}
         </>
     );
 }
